e9c0bc12646bb6529d838246ce86f86d58c4a01d
sroxck <642261686@qq.com>
2022-10-31 16:13:12 +0800

    feat: add docs

--- deploy.sh
+++ deploy.sh
@@ -19,18 +19,5 @@ git commit -m "deploy"
 git push -f git@github.com:sroxck/sroxck.github.io.git master:gh-pages  # 推送到github gh-pages分支
 git push -f https://github.com/sroxck/sroxck.github.io.git master:gh-pages  # 推送到github gh-pages分支
 https://github.com/sroxck/sroxck.github.io.git
-# deploy to coding pages
-# echo 'www.sroxck.com\nsroxck.com' > CNAME  # 自定义域名
-# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:xgy/xgy.git
-# else
-#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
-
 cd -
 rm -rf docs/.vuepress/dist
